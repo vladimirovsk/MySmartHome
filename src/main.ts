b1d3c3b812028cbedca300ada3f8d6e21f04ea6c
Serhii Vladimirov <vladimirovsk@gmail.com>
2024-06-28 11:14:30 -0600

    fix docker-compose

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = 8000
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
-  await app.listen(8000);
+  await app.listen(PORT);
 }
 
-bootstrap();
+bootstrap()
+    .then(r => {console.log(`Start server port: ${PORT}`)})
+    .catch(err=> {console.error(`Error start server in port: ${PORT}`)})
